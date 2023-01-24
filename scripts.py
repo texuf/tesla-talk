49fa35f9fee9845e967433c98bf607771e1e5afb
texuf <texuf.eth@gmail.com>
2023-01-23 21:28:47 -0800

    failed to revoke token, hopefully changing the password revokes everything

--- scripts.py
+++ scripts.py
@@ -1,12 +1,12 @@
 import os
 import random
 from twilio.rest import Client
-
+import teslapy
 
 QUOTES = [
 	"Writers are desperate people and when they stop being desperate they stop being writers.",
 	"We are here to laugh at the odds and live our lives so well that Death will tremble to take us.",
-	"Stop insisting on clearing your head—clear your fucking heart instead.",
+	"Stop insisting on clearing your head-clear your fucking heart instead.",
 	"If you have the ability to love, love yourself first.",
 	"Poetry is what happens when nothing else can.",
 	"Find what you love and let it kill you.",
@@ -18,10 +18,27 @@ QUOTES = [
 	"Beware of those who seek constant crowds; they are nothing alone.",
 	"You begin saving the world by saving one man at a time; all else is grandiose romanticism or politics.",
 	"What matters most is how well you walk through the fire.",
-	"We’re all going to die, all of us, what a circus!",
+	"We're all going to die, all of us, what a circus!",
 	"An intellectual says a simple thing in a hard way. An artist says a hard thing in a simple way."
 ]
 
+## doesn't work...
+def revoke_token(token):
+    with teslapy.Tesla('austinellis@gmail.com') as tesla:
+        print(tesla.api('STATUS'))
+        tesla.endpoints["REVOKE_TOKEN_XXX"] =  {
+            'TYPE': 'POST',
+            'URI': 'oath2/v3/revoke', 
+            'AUTH': True
+        }
+        uri = "https://auth.tesla.com/oauth2/revoke"
+        serialize = True
+        arg_name = 'json'
+        resp = tesla.request('POST', uri, serialize,
+                            withhold_token=False,
+                            **{arg_name: {'token': token}})
+        print(resp)
+        
 
 def send_sms(message):
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
@@ -42,9 +59,7 @@ def run():
     print("Hello World DEBUG_1:", os.environ.get("DEBUG_1", "Not set"))
     index = random.randint(0, len(QUOTES) - 1)
     quote = QUOTES[index]
-    print("Sending quote:", quote)
-    send_sms(quote)
-
+    
 
 if __name__ == "__main__":
     run()
